837b5438f331fcbb6739ddcfefc2cdae29520124
Thierry-Lenepveu <thierry.lenepveu.33@gmail.com>
2024-10-15 11:04:29 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk  from "chalk";
+
+console.log(chalk.blue("Julien Joecker"));
+console.log(chalk.red("Justine Cousin"));
+console.log(chalk.green("Charlotte Albouy"));
+console.log(chalk.yellow("Radwan Osman"));
